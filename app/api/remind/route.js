f880bd10ee382b1693eb901cd0d31cd6cca0c68a
Іван Калинич <ivankalynych@MacBook-Air-Ivan.local>
2024-06-05 14:16:58 +0300

    sd;faj

--- app/api/remind/route.js
+++ app/api/remind/route.js
@@ -33,9 +33,9 @@ export async function POST(req, res) {
         console.log("🚀 ~ POST ~ decryptedPassword:", decryptedPassword)
 
         const transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            port: process.env.MAIL_PORT,
-            secure: process.env.MAIL_SECURE,
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASSWORD
