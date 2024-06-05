a6c40b6a3e1bd3edf610ea19574786b4f941b266
Іван Калинич <ivankalynych@MacBook-Air-Ivan.local>
2024-06-05 14:11:09 +0300

    sa;kjf

--- app/api/remind/route.js
+++ app/api/remind/route.js
@@ -35,7 +35,7 @@ export async function POST(req, res) {
         const transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
             port: process.env.MAIL_PORT,
-            secure: process.env.MAIL_SECURE === 'true',
+            secure: process.env.MAIL_SECURE,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASSWORD
